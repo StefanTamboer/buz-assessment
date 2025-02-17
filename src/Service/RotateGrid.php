7cb8274d8b0b63f0e19a04e2b7fba190d0d209ab
stefan <stefan.tamboer@gmail.com>
2025-02-17 13:06:31 +0100

    refactor rotation

--- src/Service/RotateGrid.php
+++ src/Service/RotateGrid.php
@@ -5,24 +5,28 @@ namespace App\Service;
 class RotateGrid
 {
     /**
-     * @var array $grid
      * @return array
+     * @var array $grid
      */
     public function rotateRight(array $grid): array
     {
-        return array_map(function($row, $column_key) use ($grid){
-            return array_reverse(array_column($grid, $column_key));
-        }, $grid[0], array_keys($grid[0]));
+        return array_map(
+            fn($row, $column_key) => array_reverse(array_column($grid, $column_key)),
+            $grid[0],
+            array_keys($grid[0])
+        );
     }
 
     /**
-     * @var array $grid
      * @return array
+     * @var array $grid
      */
     public function rotateLeft(array $grid): array
     {
-        return array_map(function($row, $column_key) use ($grid){
-            return array_column($grid, count($grid[0]) - 1 -$column_key);
-        }, $grid[0], array_keys($grid[0]));
+        return array_map(
+            fn($row, $column_key) => array_column($grid, count($grid[0]) - 1 - $column_key),
+            $grid[0],
+            array_keys($grid[0])
+        );
     }
 }
